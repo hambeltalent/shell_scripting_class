3513d0203b32c58b883cf1b205e2095eeb4ea3e3
hambeltalent <hambelolas@gmail.com>
2023-08-28 12:37:32 +0100

    boss correction

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # shell_scripting_class
 This is a shell scripting class that uses github
+i
+this script will collect a dummy data and to show the importance of github and linux together
+
---
+++ datacollection.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/python3
+name = input("please enter your name:")
+age = input("please input your age:")
+phone_number = input("please enter your phone number:")
+email_address = input("please enter your email address:")
+
+print(name)
+print(age)
+print(phone_number)
+print(email_address)
+
