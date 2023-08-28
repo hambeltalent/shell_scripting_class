5bfb7473eeaff89258a4b470f826a1be20a88ebd
hambeltalent <hambelolas@gmail.com>
2023-08-28 15:45:55 +0100

    Check out my first practice

---
+++ My_first_ practice.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+print("command to print 10 lines from top to bottom is head")
+
+print("command to search through a file is greg")
+
+print("how to create an empty file is touch or Vi")
+
+print("command to move and copy a file is mv and cp")
+
+print("command to check in a file is CAT")
